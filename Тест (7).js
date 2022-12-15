4469c4150be5e5837759b3c1bf40c5f9608a6f7e
Ziyawwd <114824886+Ziyawwd@users.noreply.github.com>
2022-12-15 21:33:32 +0400

    Add files via upload

---
+++ Тест (7).js
@@ -0,0 +1,115 @@
+// 1)
+//перечисли все типы данных
+// Number
+// object
+// function
+// String
+// null
+// 2)
+//перечисли типы объектов
+
+// 3)
+//какие типы данных не могут содержать значения
+// null undefined NaN
+// 4)
+//выведите в консоли тип данных ниже
+let a = 'John';
+let b = 3.14;
+let c = NaN;
+let d = false;
+let e = [1, 2, 3, 4];
+let f = {name: 'ALex', age: 36}
+let q = new Date();
+let r = function () {};
+let k;
+let n = null;
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+console.log(typeof(q))
+console.log(typeof(r))
+console.log(typeof(k))
+console.log(typeof(n))
+console.log("_________")
+// 5)
+//преобразуйте строки ниже в числа c помощью Number
+ a = '23.5';
+ b = '20';
+ d = '0000000.1';
+c = ' ';
+n = 'hello';
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
+console.log(Number(n));
+console.log("_________")
+// 6)
+//преобразуйте строки ниже в числа c помощью унарного плюса
+ a = '23.5';
+ b = '20';
+ d = '0000000.1';
+ c = ' ';
+ n = 'hello';
+ console.log("_________")
+// 7)
+//вырежте из строк ниже целую часть числа и выведите в консоли
+ a = '23.5px';
+ b = '20rem';
+ d = '0000000.1deg';
+
+
+ console.log("_________")
+// 8)
+//вырежте из строк ниже числа вместе с плавающей точкой и выведите в консоли
+ a = '23.5px';
+ b = '20rem';
+ d = '0000000.1deg';
+ console.log("_________")
+// 9)
+//преобразуйте переменные ниже в строки с помощью String
+ a = 234;
+ b = true;
+ console.log(String(a));
+ console.log(String(b));
+ console.log("_________")
+// 10)
+//преобразуйте переменные ниже в строки с помощью toString
+ a = 234;
+ b = true;
+ console.log(toString(a));
+ console.log(toString(b));
+ console.log("_________")
+//11)
+//преобразуйте дату в число и выведите в консоли
+let dat = new Date();
+console.log(Number(dat));
+console.log("_________")
+// 12)
+//что выведет в консоли 
+a = 5 + null;
+// либо нан или  undefined
+console.log("_________")
+//13)
+//что выведет в консоли 
+b = '5' + null;
+// 5null
+console.log("_________")
+// 14)
+//что выведет в консоли 
+c = '5' + 2;
+// 52
+console.log("_________")
+// 15)
+//что выведет в консоли 
+d = '5' - 2;
+// nan
+console.log("_________")
+// 16) 
+//что выведет в консоли 
+r = '5' * 2;
+// nan
+console.log("-_________-")
