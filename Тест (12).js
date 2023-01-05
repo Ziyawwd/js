2bac2b8bcb2a643162ac9ec71150afc9ce0fe478
Ziyawwd <114824886+Ziyawwd@users.noreply.github.com>
2023-01-05 18:32:32 +0400

    Add files via upload

---
+++ Тест (12).js
@@ -0,0 +1,22 @@
+1)
+//в чем + стрелочных функций?
+// они укорачивают код
+2)
+//превратите функцию ниже в стрелочную
+newUser = (name, age) =>  {
+    if (age > 18) {
+        console.log(`${name} ты стар`);
+    } else {
+        console.log(`${name} ты сликом мал`);
+    } 
+
+}
+
+3)
+//сделай скрипт ниже короче
+call = (a,b) => return a + b
+// 56 bonus
+
+function sortrovka(arr1) {
+    return arr1.filter()
+}
